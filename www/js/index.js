43cba873293ea7c1744256ae7c222f1841f8b73d
Nicolas <nicolas@waynium.com>
2019-07-02 10:56:27 +0200

    Test permissions

--- www/js/index.js
+++ www/js/index.js
@@ -38,13 +38,11 @@ var app = {
 
         //permissions.requestPermission(permissions.LOCATION, success, error);
         permissions.requestPermissions([
-            permissions.LOCATION,
             permissions.ACCESS_BACKGROUND_LOCATION,
             permissions.ACCESS_COARSE_LOCATION,
             permissions.ACCESS_FINE_LOCATION
         ], success, error);
 
-        permissions.checkPermission(permissions.LOCATION, success, error);
         permissions.checkPermission(permissions.ACCESS_BACKGROUND_LOCATION, success, error);
         permissions.checkPermission(permissions.ACCESS_COARSE_LOCATION, success, error);
         permissions.checkPermission(permissions.ACCESS_FINE_LOCATION, success, error);
